fd1497170d1b35fca50a9e88c3694d7ce6b4f596
Naman <namansharmaves@gmail.com>
2024-09-04 02:20:47 +0530

    setup.py and requiremnt

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT= '-e .'
+def get_requirement(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirement 
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='AIML_Project',
+version='0.0.1',
+author='Naman Sharma',
+author_email='namansharmaves@gmail.com',
+packages=find_packages(),
+install_requires = get_requirement('requirements.txt')
+)
